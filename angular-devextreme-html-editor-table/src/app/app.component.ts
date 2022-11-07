165737114f7bef652901157e70bb1dfd94e99e51
DUNG5 NGUYEN TIEN <ntdung5@misa-group.vn>
2022-11-07 09:02:09 +0700

    devextreme html editor table

---
+++ angular-devextreme-html-editor-table/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { DxHtmlEditorModule, DxCheckBoxModule } from 'devextreme-angular';
+
+import { Service } from './app.service';
+
+
+@Component({
+  selector: 'demo-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [Service],
+})
+
+export class AppComponent {
+  valueContent: string;
+
+  allowResizing = true;
+
+  contextMenuEnabled = true;
+
+  constructor(service: Service) {
+    this.valueContent = service.getMarkup();
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    DxHtmlEditorModule,
+    DxCheckBoxModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
